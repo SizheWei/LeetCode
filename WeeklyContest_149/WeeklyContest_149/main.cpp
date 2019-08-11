657c0cc0fce6cca581bf24e53b57b5b6cf278548
Sizhe Wei <sizhewei@sjtu.edu.cn>
2019-08-11 12:03:33 +0800

    join the 149 weekly contest

---
+++ WeeklyContest_149/WeeklyContest_149/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WeeklyContest_149
+//
+//  Created by 魏思哲 on 11/8/19.
+//  Copyright © 2019 SizheWei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
