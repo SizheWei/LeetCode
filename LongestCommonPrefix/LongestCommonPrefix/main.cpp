502e90168ffacd489381e7c8c063d8ffe8b8b7bc
Sizhe Wei <sizhewei@sjtu.edu.cn>
2019-08-14 10:34:30 +0800

    finish problem # 14

--- LongestCommonPrefix/LongestCommonPrefix/main.cpp
+++ LongestCommonPrefix/LongestCommonPrefix/main.cpp
@@ -8,19 +8,38 @@
 //  leetcode # 14
 
 #include <iostream>
-
+#include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string result;
-        
+        string result="";
+        if(strs.size()!=0){
+            string tmp=strs[0];
+            for(int j=0;j<strs.size();j++){
+                int i=0;
+                result="";
+                while(i<min(tmp.size(),strs[j].size())){
+                    if(tmp[i]==strs[j][i]){
+                        result=result+tmp[i];
+                        i++;
+                    }
+                    else break;
+                }
+                tmp=result;
+            }
+        }
         return result;
     }
 };
 
 int main() {
-    
+    Solution problem;
+    string result;
+    vector<string> prem = {"a"};
+    result = problem.longestCommonPrefix(prem);
+    cout<<result<<endl;
     return 0;
 }
